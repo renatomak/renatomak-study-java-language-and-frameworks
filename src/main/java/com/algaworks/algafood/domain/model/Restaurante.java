a44d3f8578a5dddd16d780128aae21eb841ead54
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-01-24 20:16:21 -0300

    first commit

---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_restaurante")
+public class Restaurante {
+	
+	@Id
+	private Long id;
+	
+	private String nome;
+	
+	private BigDecimal taxaFrete;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getTaxaFrete() {
+		return taxaFrete;
+	}
+
+	public void setTaxaFrete(BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+	
+	
+
+}
