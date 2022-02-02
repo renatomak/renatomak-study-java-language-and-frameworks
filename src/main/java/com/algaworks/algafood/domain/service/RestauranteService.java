6caba6d958c7cbc8d10e44e365063eaba8d9eed2
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-02-01 22:30:42 -0300

    Inicio do modulo 5 - super poderes do Spring Data Jpa

---
+++ src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
@@ -0,0 +1,41 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.infrastructure.repository.RestauranteRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RestauranteService {
+
+    @Autowired
+    private RestauranteRepositoryImpl restauranteRepository;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    public Restaurante salvar(Restaurante restaurante) {
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException(
+                        String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
+
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.salvar(restaurante);
+    }
+
+    public void remover(Restaurante restaurante) {
+        try {
+            restauranteRepository.remover(restaurante);
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe um cadastro de restaurante com código %d", restaurante.getId()));
+        }
+    }
+
+}
