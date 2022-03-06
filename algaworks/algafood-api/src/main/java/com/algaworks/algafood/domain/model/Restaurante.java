ba3736750da8bfe4b91a19da3341eb366c830fd5
renatomak <renato.mark.silva@gmail.com>
2022-03-06 07:26:36 -0300

    atualizar projeto

--- algaworks/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algaworks/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -71,4 +71,9 @@ public class Restaurante {
 	@OneToMany(mappedBy = "restaurante")
 	private List<Produto> produtos = new ArrayList<>();
 
+	@ManyToMany
+	@JoinTable(name = "restaurante_usuario", joinColumns = @JoinColumn(name = "restaurante_id"),
+	inverseJoinColumns = @JoinColumn(name = "usuario_id"))
+	private List<Usuario> usuarios = new ArrayList<>();
+
 }
