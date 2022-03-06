2e9e8b5851c8933046529c9f3c073e31feef4985
renatomak <renato.mark.silva@gmail.com>
2022-03-06 11:44:23 -0300

    table products

---
+++ hands-on/store-manager/src/main/java/br/com/handson/store/model/Product.java
@@ -0,0 +1,24 @@
+package br.com.handson.store.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity = 0;
+}
